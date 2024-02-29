081089edfe46873a50b4affb1a3be74bb8862d2f
praveen <praveen.varaprasad@gmail.com>
2024-02-29 14:42:17 +0530

    second project

---
+++ app/src/main/java/com/synycs/test2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.synycs.test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toast.makeText(this, "Hello 2", Toast.LENGTH_SHORT).show();
+    }
+}
